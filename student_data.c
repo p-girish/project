cacfb49e69661f423e6d75affdaa6df9ed46f2a3
POTTA GIRISH <p.girish1122@gmail.com>
2023-06-23 07:51:01 +0000

    multiple students and calculates their total marks and percentage

--- student_data.c
+++ student_data.c
@@ -1,52 +1,49 @@
 #include <stdio.h>
 
 struct Student {
-	char name[50];
-	int rollNumber;
-	int marks[5];
-	int totalMarks;
-	float percentage;
+        char name[50];
+        int rollNumber;
+        int marks[5];
+        int totalMarks;
+        float percentage;
 };
 
 int main() {
-	int numStudents;
+        int numStudents;
 
-	printf("Enter the number of students: ");
-	scanf("%d", &numStudents);
+        printf("Enter the number of students: ");
+        scanf("%d", &numStudents);
 
-	struct Student students[numStudents];
+        struct Student students[numStudents];
 
-	// Input student data
-	for (int i = 0; i < numStudents; i++) {
-		printf("\nStudent %d:\n", i + 1);
-		printf("Enter name: ");
-		scanf("%s", students[i].name);
+        // Input student data
+        for (int i = 0; i < numStudents; i++) {
+                printf("\nStudent %d:\n", i + 1);
+                printf("Enter name: ");
+                scanf("%s", students[i].name);
 
-		printf("Enter roll number: ");
-		scanf("%d", &students[i].rollNumber);
+                printf("Enter roll number: ");
+                scanf("%d", &students[i].rollNumber);
 
-		printf("Enter marks for 5 subjects:\n");
-		students[i].totalMarks = 0;
-		for (int j = 0; j < 5; j++) {
-			printf("Subject %d: ", j + 1);
-			scanf("%d", &students[i].marks[j]);
-			students[i].totalMarks += students[i].marks[j];
-		}
+                printf("Enter marks for 5 subjects:\n");
+                students[i].totalMarks = 0;
+                for (int j = 0; j < 5; j++) {
+                        printf("Subject %d: ", j + 1);
+                        scanf("%d", &students[i].marks[j]);
+                        students[i].totalMarks += students[i].marks[j];
+                }
 
-		students[i].percentage = (float) students[i].totalMarks / 5;
-	}
+                students[i].percentage = (float) students[i].totalMarks / 5;
+        }
+        // Display student data
+        printf("\n\nStudent Data:\n");
+        for (int i = 0; i < numStudents; i++) {
+                printf("\nStudent %d:\n", i + 1);
+                printf("Name: %s\n", students[i].name);
+                printf("Roll Number: %d\n", students[i].rollNumber);
+                printf("Total Marks: %d\n", students[i].totalMarks);
+                printf("Percentage: %.2f%%\n", students[i].percentage);
+        }
 
-	// Display student data
-	printf("\n\nStudent Data:\n");
-	for (int i = 0; i < numStudents; i++) {
-		printf("\nStudent %d:\n", i + 1);
-		printf("Name: %s\n", students[i].name);
-		printf("Roll Number: %d\n", students[i].rollNumber);
-		printf("Total Marks: %d\n", students[i].totalMarks);
-		printf("Percentage: %.2f%%\n", students[i].percentage);
-	}
-
-	return 0;
+        return 0;
 }
-
-
